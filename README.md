9d7d3f90cdc2eb1c6fb9f905b278067347d91d9b
sid-jadhav <sid.m.jadhav@gmail.com>
2019-10-21 19:48:22 +0530

    updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# JSONPlaceHolder-API-Automation
+## JSONPlaceHolder-API-Automation
 API Automation Framework using- Java, JUnit5, Spring, Maven and RestAssured
+
+###Execution paramerters
+####Maven
+```sh 
+mvn clean test -Denvironment=dev
+```
+
+###Overview
+- Test mentioned in challenge document is automated in file FreeNowApiChallengeTest.java present in testing package.
+- Also covered positive and negative scenarios to test other workflows.
+
+
--- src/test/java/com/jsonplaceholder/testing/FreeNowApiChallengeTest.java
+++ src/test/java/com/jsonplaceholder/testing/FreeNowApiChallengeTest.java
@@ -3,15 +3,9 @@ package com.jsonplaceholder.testing;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import com.jsonplaceholder.testing.comments.Comments;
-import com.jsonplaceholder.testing.posts.Posts;
-import com.jsonplaceholder.testing.users.Users;
 
 @Tag("FreeNowApiChallenge")
 public class FreeNowApiChallengeTest extends BaseTest{
@@ -19,7 +13,6 @@ public class FreeNowApiChallengeTest extends BaseTest{
 protected static Logger LOG = Logger.getLogger(FreeNowApiChallengeTest.class);
 	
 	@Test
-	@Tag("sid")
 	@DisplayName("Validate the format of Emails in comments section for all posts of a user")
 	public void testValidateFormatOfEmailsInCommentsSectionForAllPostsOfAUser() {
 		users.getResponseByUserName("Samantha");
